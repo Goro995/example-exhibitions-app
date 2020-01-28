ccf1ca66019f9afe7676b698f987ae96d0ccb55a
Igor Gorovoy <igor@appdev.academy>
2020-01-28 18:58:01 +0200

    Modules initialization

---
+++ application/src/main/java/goro/igor/exampleexhibitionapp/MainExhibitionActivity.kt
@@ -0,0 +1,13 @@
+package goro.igor.exampleexhibitionapp
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainExhibitionActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.exhibition_layout)
+
+    }
+}
